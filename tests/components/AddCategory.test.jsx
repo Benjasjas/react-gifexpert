f7c48cf33fc7052f289e0784aeb166d4b98d1c65
benjamin bustos <benjamin.bustos@sumeria.ai>
2024-05-16 18:26:14 -0400

    feat: Add unit test for submitting the AddCategory form

--- tests/components/AddCategory.test.jsx
+++ tests/components/AddCategory.test.jsx
@@ -13,9 +13,9 @@ describe("Pruebas en AddCategory", () => {
 
   test("Debe de llamar onNewCategory si el input tiene un valor ", () => {
     const inputValue = "Luffy";
-    //TODO: ????
+    const onNewCategory = jest.fn();
 
-    render(<AddCategory onNewCategory={() => {}} />);
+    render(<AddCategory onNewCategory={onNewCategory} />);
 
     const input = screen.getByRole("textbox");
     const form = screen.getByRole("form");
@@ -24,5 +24,20 @@ describe("Pruebas en AddCategory", () => {
     fireEvent.submit(form);
 
     expect(input.value).toBe("");
+    expect(onNewCategory).toHaveBeenCalled();
+    expect(onNewCategory).toHaveBeenCalledTimes(1);
+    expect(onNewCategory).toHaveBeenCalledWith(inputValue);
+  });
+
+  test("No debe de llamar el onNewCategory si el input está vacio", () => {
+    const onNewCategory = jest.fn();
+    render(<AddCategory onNewCategory={onNewCategory} />);
+
+    const form = screen.getByRole("form");
+    fireEvent.submit(form);
+
+    expect(onNewCategory).not.toHaveBeenCalled();
+    expect(onNewCategory).toHaveBeenCalledTimes(0);
+    expect(onNewCategory).not.toHaveBeenCalledWith("");
   });
 });
