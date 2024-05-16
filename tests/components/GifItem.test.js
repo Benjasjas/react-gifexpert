8113399a1e17d43a886213884b95056cb37e70ab
benjamin bustos <benjamin.bustos@sumeria.ai>
2024-05-16 17:16:06 -0400

    refactor: Update GifItem.test.js to include tests for image URL and title display

--- tests/components/GifItem.test.js
+++ tests/components/GifItem.test.js
@@ -1,4 +1,4 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { GifItem } from "../../src/components/GifItem";
 
 describe("Prueba en <GifItem/>", () => {
@@ -8,5 +8,21 @@ describe("Prueba en <GifItem/>", () => {
   test("debe hacer match con el snapshot", () => {
     const { container } = render(<GifItem title={title} url={url} />);
     expect(container).toMatchSnapshot();
+  });
+
+  test("debe de mostrar la imagen con el URL y el ALT indicado", () => {
+    render(<GifItem title={title} url={url} />);
+    // screen.debug();
+    // expect(screen.getByRole("img").src).toBe(url);
+    // expect(screen.getByRole("img").alt).toBe(title);
+
+    const { src, alt } = screen.getByRole("img");
+    expect(src).toBe(url);
+    expect(alt).toBe(title);
+  });
+
+  test("debe de mostrar el titulo en el componente", () => {
+    render(<GifItem title={title} url={url} />);
+    expect(screen.getByText(title)).toBeTruthy();
   });
 });
