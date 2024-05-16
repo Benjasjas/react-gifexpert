ed92d3bd9dd270d32fac154501da583fb05cdd77
benjamin bustos <benjamin.bustos@sumeria.ai>
2024-05-16 17:17:56 -0400

    refactor: Remove commented code and clean up getGifs helper

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,28 +1,5 @@
 export const getGifs = async (category) => {
   const url = `https://api.giphy.com/v1/gifs/search?api_key=FuquiPoCz3sKT3QPXtw8PKIqfiRnHg9M&q=${category}&limit=10`;
-
-  //   try {
-  //     const response = await fetch(url);
-
-  //     if (!response.ok) {
-  //       if (response.status === 429) {
-  //         throw new Error("Too many requests");
-  //       } else {
-  //         throw new Error("Error fetching gifs");
-  //       }
-  //     }
-
-  //     const { data } = await response.json();
-  //     return data.map((img) => ({
-  //       id: img.id,
-  //       title: img.title,
-  //       url: img.images.downsized_medium.url,
-  //     }));
-  //   } catch (error) {
-  //     console.error(error);
-  //     throw error;
-  //   }
-
   const resp = await fetch(url);
   const { data } = await resp.json();
 
